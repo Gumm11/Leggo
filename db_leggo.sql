8ed6b90bbc924d8812489f7dea41f995ac9697b2
Gumm11 <vaaa7673@gmail.com>
2024-07-10 21:20:28 +0700

    samting samting

--- db_leggo.sql
+++ db_leggo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 09, 2024 at 04:28 AM
+-- Generation Time: Jul 10, 2024 at 02:23 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -74,17 +74,56 @@ INSERT INTO `kota` (`Id_kota`, `Nama_kota`, `Negara`, `Tempat_wisata`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `passengers`
+--
+
+CREATE TABLE `passengers` (
+  `id` int(11) NOT NULL,
+  `id_pemesanan` int(11) NOT NULL,
+  `nama_lengkap` varchar(100) NOT NULL,
+  `age_group` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `passengers`
+--
+
+INSERT INTO `passengers` (`id`, `id_pemesanan`, `nama_lengkap`, `age_group`) VALUES
+(1, 1, 'Vovella', 'dewasa'),
+(2, 1, 'Angelin', 'dewasa'),
+(3, 2, 'Vovella', 'dewasa'),
+(4, 2, 'Angelin', 'anak-anak'),
+(5, 3, 'Vovella', 'dewasa'),
+(6, 3, 'Angelin', 'anak-anak');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pemesanan`
 --
 
 CREATE TABLE `pemesanan` (
-  `Id_pemesanan` int(11) NOT NULL,
-  `Id_penerbangan` int(11) NOT NULL,
-  `Id_users` int(11) NOT NULL,
-  `Jumlah_penumpang` int(11) NOT NULL,
-  `Total_bayar` decimal(10,2) NOT NULL,
-  `Status_pembayaran` varchar(20) NOT NULL
+  `id_pemesanan` int(11) NOT NULL,
+  `id_penerbangan_berangkat` int(11) DEFAULT NULL,
+  `id_penerbangan_pulang` int(11) DEFAULT NULL,
+  `id_users` int(11) DEFAULT NULL,
+  `jumlah_penumpang` int(11) DEFAULT NULL,
+  `total_bayar` decimal(10,2) DEFAULT NULL,
+  `status_pembayaran` varchar(50) DEFAULT NULL,
+  `nama_lengkap` varchar(100) DEFAULT NULL,
+  `nomor_telepon` varchar(20) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `selected_bagasi` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pemesanan`
+--
+
+INSERT INTO `pemesanan` (`id_pemesanan`, `id_penerbangan_berangkat`, `id_penerbangan_pulang`, `id_users`, `jumlah_penumpang`, `total_bayar`, `status_pembayaran`, `nama_lengkap`, `nomor_telepon`, `email`, `selected_bagasi`) VALUES
+(1, 1, 7, 1, 2, 10467000.00, 'Pending', 'Vovella', '082360988114', '03082220006@student.uph.edu', 10),
+(2, 7, 1, 1, 2, 10467000.00, 'Success', 'Vovella', '082360988114', '03082220006@student.uph.edu', 10),
+(3, 7, 1, 1, 2, 10467000.00, 'Pending', 'Vovella', '082360988114', '03082220006@student.uph.edu', 10);
 
 -- --------------------------------------------------------
 
@@ -110,8 +149,8 @@ CREATE TABLE `penerbangan` (
 --
 
 INSERT INTO `penerbangan` (`Id_penerbangan`, `Id_pesawat`, `Id_bandara_keberangkatan`, `Id_bandara_kedatangan`, `Tanggal_keberangkatan`, `Jam_keberangkatan`, `Jam_kedatangan`, `Harga_tiket`, `Jumlah_kursi`, `Kelas`) VALUES
-(1, 1, 6, 2, '2024-05-08', '05:25:00', '14:35:00', 2296750.00, 6, 'Ekonomi'),
-(2, 2, 6, 2, '2024-05-08', '05:50:00', '11:40:00', 2396750.00, 8, 'Ekonomi'),
+(1, 1, 6, 2, '2024-10-08', '05:25:00', '14:35:00', 2296750.00, 6, 'Ekonomi'),
+(2, 2, 6, 2, '2024-10-08', '05:50:00', '11:40:00', 2396750.00, 8, 'Ekonomi'),
 (3, 3, 6, 2, '2024-04-10', '13:50:00', '20:10:00', 2796750.00, 9, 'Ekonomi'),
 (4, 3, 6, 2, '2024-04-10', '04:55:00', '18:05:00', 3296750.00, 10, 'Ekonomi'),
 (7, 2, 2, 6, '2024-07-10', '10:50:00', '11:40:00', 2436750.00, 10, 'Ekonomi');
@@ -179,12 +218,20 @@ ALTER TABLE `kota`
   ADD PRIMARY KEY (`Id_kota`);
 
 --
+-- Indexes for table `passengers`
+--
+ALTER TABLE `passengers`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_pemesanan` (`id_pemesanan`);
+
+--
 -- Indexes for table `pemesanan`
 --
 ALTER TABLE `pemesanan`
-  ADD PRIMARY KEY (`Id_pemesanan`),
-  ADD KEY `Id_penerbangan` (`Id_penerbangan`),
-  ADD KEY `Id_pelanggan` (`Id_users`);
+  ADD PRIMARY KEY (`id_pemesanan`),
+  ADD KEY `id_penerbangan_berangkat` (`id_penerbangan_berangkat`),
+  ADD KEY `id_penerbangan_pulang` (`id_penerbangan_pulang`),
+  ADD KEY `id_users` (`id_users`);
 
 --
 -- Indexes for table `penerbangan`
@@ -224,10 +271,16 @@ ALTER TABLE `kota`
   MODIFY `Id_kota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT for table `passengers`
+--
+ALTER TABLE `passengers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT for table `pemesanan`
 --
 ALTER TABLE `pemesanan`
-  MODIFY `Id_pemesanan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pemesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `penerbangan`
@@ -258,11 +311,18 @@ ALTER TABLE `bandara`
   ADD CONSTRAINT `bandara_ibfk_1` FOREIGN KEY (`Id_kota`) REFERENCES `kota` (`Id_kota`);
 
 --
+-- Constraints for table `passengers`
+--
+ALTER TABLE `passengers`
+  ADD CONSTRAINT `passengers_ibfk_1` FOREIGN KEY (`id_pemesanan`) REFERENCES `pemesanan` (`id_pemesanan`);
+
+--
 -- Constraints for table `pemesanan`
 --
 ALTER TABLE `pemesanan`
-  ADD CONSTRAINT `pemesanan_ibfk_1` FOREIGN KEY (`Id_penerbangan`) REFERENCES `penerbangan` (`Id_penerbangan`),
-  ADD CONSTRAINT `pemesanan_ibfk_2` FOREIGN KEY (`Id_users`) REFERENCES `pelanggan` (`Id_pelanggan`);
+  ADD CONSTRAINT `pemesanan_ibfk_1` FOREIGN KEY (`id_penerbangan_berangkat`) REFERENCES `penerbangan` (`Id_penerbangan`),
+  ADD CONSTRAINT `pemesanan_ibfk_2` FOREIGN KEY (`id_penerbangan_pulang`) REFERENCES `penerbangan` (`Id_penerbangan`),
+  ADD CONSTRAINT `pemesanan_ibfk_3` FOREIGN KEY (`id_users`) REFERENCES `users` (`Id_users`);
 
 --
 -- Constraints for table `penerbangan`
